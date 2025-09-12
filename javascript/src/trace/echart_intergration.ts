ffdb8d4d1381d23f2d8609241781c874ba0933ed
Eric_Lian <git@superexercisebook.com>
2025-09-12 14:41:17 +0800

    chores: removes echarts dependency

--- javascript/src/trace/echart_intergration.ts
+++ javascript/src/trace/echart_intergration.ts
@@ -1,37 +1,7 @@
 import type {OverlapResult, TraceEvent} from "./TraceFile.ts";
 import type {Interval} from "./types.ts";
-import * as echarts from "echarts/core";
 import {eta} from "../template/index.ts"
 import fs from "fs";
-
-function renderItem(params, api) {
-    const categoryIndex = api.value(0);
-    const start = api.coord([api.value(1), categoryIndex]);
-    const end = api.coord([api.value(2), categoryIndex]);
-    const height = api.size([0, 1])[1] * 0.6;
-    const rectShape = echarts.graphic.clipRectByRect(
-        {
-            x: start[0],
-            y: start[1] - height / 2,
-            width: end[0] - start[0],
-            height: height
-        },
-        {
-            x: params.coordSys.x,
-            y: params.coordSys.y,
-            width: params.coordSys.width,
-            height: params.coordSys.height
-        }
-    );
-    return (
-        rectShape && {
-            type: 'rect',
-            transition: ['shape'],
-            shape: rectShape,
-            style: api.style()
-        }
-    );
-}
 
 export type EchartTraceOverlapOption = {
     normalizeTime?: boolean; // 是否归一化时间轴到0开始
@@ -62,7 +32,7 @@ export function makeTraceOverlapOption(data: OverlapResult, cfg: EchartTraceOver
             value: [0, st, et, (et - st)],
             itemStyle: {
                 normal: {
-                    color: "#75d874"
+                    color: "#74d5d8"
                 }
             }
         })
