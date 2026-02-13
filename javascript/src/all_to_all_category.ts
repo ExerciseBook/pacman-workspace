f14d1f965601f59e72a24f75221f5f981614e154
Eric_Lian <git@superexercisebook.com>
2026-02-13 15:44:49 +0800

    feat: add all-to-all category analysis

---
+++ javascript/src/all_to_all_category.ts
@@ -0,0 +1,61 @@
+import {type TraceEvent, TraceFile} from './trace/TraceFile.ts'
+
+const trace = 'C:\\Users\\mo\\work\\pacman-workspace\\javascript\\data\\ep\\ep8\\torch_prof_aibenchmark_tp2_pp1_ep8_etp1_vp\\trace_rank0_step11.json'
+const traceFile = new TraceFile(trace);
+
+function getStack(event: TraceEvent): any {
+    const externalId = event.args["External id"]
+
+    const markers = traceFile.filterEvent(s => !!s.args && s.args["External id"] == externalId && s.cat == "user_annotation");
+    if (markers.length != 1) {
+        throw new Error(`Expected exactly one user_annotation event with External id ${externalId}, but found ${s.length}`);
+    }
+
+    const marker = markers[0]!;
+
+    // console.log(s)
+
+    if (!marker.ts) {
+        throw new Error(`Marker event does not have a timestamp`);
+    }
+
+    const result = traceFile.filterEvent(s => {
+        if (s.tid != marker.tid) {
+            return false
+        }
+
+        if (!s.ts) {
+            return false
+        }
+
+        if (s.ts > marker.ts!) {
+            return false
+        }
+
+        if (!s.dur) {
+            return false // 存疑
+        }
+
+        if (s.ts + s.dur < marker.ts!) {
+            return false
+        }
+
+        return true
+    }).sort((a, b) => a.ts! - b.ts!)
+
+    return result
+}
+
+const result = traceFile.filterEvent(
+    (item) => {
+        return item.name == "nccl:all_to_all" && item.cat == 'gpu_user_annotation'
+    }
+);
+
+// for (let item of result) {
+//     const s = getStack(item);
+// }
+
+console.log(getStack(result[0]).map(s => s.name))
+
+// console.log(result);
